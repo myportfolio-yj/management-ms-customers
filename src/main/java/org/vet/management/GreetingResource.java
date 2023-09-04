27ebd26f1c651862c476370f4eab38f43db931c9
Yaquelin Jimenez <gemmay26gmail.com>
2023-09-04 13:39:54 -0500

    Configuracion completa backend para el primer 'hola mundo'

--- src/main/java/org/vet/management/GreetingResource.java
+++ src/main/java/org/vet/management/GreetingResource.java
@@ -4,14 +4,15 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api")
 public class GreetingResource {
 
     @GET
     @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hola Mundo";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        return Response.ok("{\"message\": \"Hola Mundo\"}").build();
     }
 }
